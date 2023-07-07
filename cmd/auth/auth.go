aa0cb4385cb60f978665e04158b303cc845793bc
Edward Agombar <ejagombar@gmail.com>
2023-07-07 22:50:44 +0200

    added spotify auth example to auth

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -1,13 +1,66 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package auth
 
 import (
+	"context"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/zmb3/spotify/v2"
+	"github.com/zmb3/spotify/v2/auth"
+	"log"
+	"net/http"
 )
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+}
+
+func startServer() {
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
 
 // authCmd represents the auth command
 var AuthCmd = &cobra.Command{
@@ -15,7 +68,7 @@ var AuthCmd = &cobra.Command{
 	Short: "Connect your spotify account",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("auth called")
+		startServer()
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/ejagombar/CLSpotify
 go 1.20
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/zmb3/spotify/v2 v2.3.1 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
