cd263c905842b11aa33ac014b844454b249b23c7
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2023-07-11 11:56:08 +0100

    add html response page

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -17,9 +17,29 @@ import (
 const redirectURI = "http://localhost:8080/callback"
 
 var (
-	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
-	ch    = make(chan *spotify.Client)
-	state = "abc123"
+	auth   *spotifyauth.Authenticator
+	ch     = make(chan *spotify.Client)
+	state  = "abc123"
+	client *spotify.Client
+	form   = `<!DOCTYPE html>
+<html>
+<head>
+<!-- HTML Codes by Quackit.com -->
+<title>
+Login Complete!</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {background-color:#000000;background-repeat:no-repeat;background-position:top left;background-attachment:fixed;}
+h1{text-align:center;font-family:Helvetica, sans-serif;color:#ffffff;background-color:#000000;}
+p {text-align:center;font-family:Helvetica, sans-serif;font-size:18px;font-style:normal;font-weight:normal;color:#ffffff;background-color:#000000;}
+</style>
+</head>
+<body>
+<h1>Login Complete!</h1>
+<p>Please return to your terminal to continue</p>
+</body>
+</html>
+`
 )
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
@@ -34,13 +54,14 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// use the token to get an authenticated client
-	client := spotify.New(auth.Client(r.Context(), tok))
-	fmt.Fprintf(w, "Login Completed!")
+	client = spotify.New(auth.Client(r.Context(), tok))
+	w.Header().Set("Content-Type", "text/html; charset=utf8")
+	fmt.Fprintf(w, form)
+
 	ch <- client
 }
 
-func startServer() error {
-	// first start an HTTP server
+func getClient() (client *spotify.Client, err error) {
 	http.HandleFunc("/callback", completeAuth)
 	go func() {
 		err := http.ListenAndServe(":8080", nil)
@@ -52,15 +73,28 @@ func startServer() error {
 	url := auth.AuthURL(state)
 	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
 
-	// wait for auth to complete
-	client := <-ch
+	client = <-ch
 
-	// use the client to make calls that require authorization
-	user, err := client.CurrentUser(context.Background())
-	if err != nil {
-		return err
-	}
-	fmt.Println("You are logged in as:", user.ID)
+	return client, nil
+}
+
+func GetClient() *spotify.Client {
+	return client
+}
+
+func createRequest() error {
+	spotify_id := viper.GetString("auth.client_id")
+	spotify_client := viper.GetString("auth.client_secret")
+
+	auth = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithClientID(spotify_id),
+		spotifyauth.WithClientSecret(spotify_client),
+		spotifyauth.WithScopes(
+			spotifyauth.ScopeUserReadPrivate,
+			spotifyauth.ScopeStreaming,
+			spotifyauth.ScopeUserFollowRead,
+			spotifyauth.ScopeUserModifyPlaybackState))
+
 	return nil
 }
 
@@ -70,11 +104,13 @@ var AuthCmd = &cobra.Command{
 	Short: "Connect your spotify account",
 	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		err := startServer()
-		return err
+		createRequest()
+		client2, _ := getClient()
+		user, _ := client2.CurrentUser(context.Background())
+		fmt.Println(user.DisplayName)
+		return nil
 	},
 }
 
 func init() {
-	fmt.Println(viper.GetString("auth.spotify_id"))
 }
