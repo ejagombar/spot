db73e74818afdc7faf7e6bc69222ebe7f5b5a8f5
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2023-07-17 20:13:07 +0100

    add queue flag

--- cmd/findmusic/song.go
+++ cmd/findmusic/song.go
@@ -12,36 +12,41 @@ import (
 )
 
 // pauseCmd represents the pause command
-var SongCmd = &cobra.Command{
-	Use:   "song",
-	Short: "Specifies the search for songs",
-	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		client, err := authstore.GetClient()
-		prechecks.DeviceAvailable(client)
+var (
+	queueSong bool = false
+	SongCmd        = &cobra.Command{
+		Use:   "song",
+		Short: "Specifies the search for songs",
+		Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
+			client, err := authstore.GetClient()
+			prechecks.DeviceAvailable(client)
 
-		deviceID, err := common.SelectDevice(client)
-		cobra.CheckErr(err)
+			deviceID, err := common.SelectDevice(client)
+			cobra.CheckErr(err)
 
-		if len(args) > 0 {
-			SearchSongAndPlay(client, deviceID, common.ConcatArgs(args))
-		} else {
-			err = errors.New("Song name not provided")
-		}
-		cobra.CheckErr(err)
-	},
-}
+			if len(args) > 0 {
+				SearchSongAndPlay(client, deviceID, common.ConcatArgs(args), queueSong)
+			} else {
+				err = errors.New("Song name not provided")
+			}
+			cobra.CheckErr(err)
+		},
+	}
+)
 
-func SearchSongAndPlay(client *spotify.Client, deviceid spotify.ID, songname string) (err error) {
+func SearchSongAndPlay(client *spotify.Client, deviceid spotify.ID, songname string, queueSong bool) (err error) {
 	result, err := client.Search(context.Background(), songname, spotify.SearchType(spotify.SearchTypeTrack), spotify.Limit(1))
 	if err != nil {
 		return fmt.Errorf("Error while searching for song: %w", err)
 	}
 
-	uri := result.Tracks.Tracks[0].URI
-	opts := spotify.PlayOptions{DeviceID: &deviceid, URIs: []spotify.URI{uri}}
-	err = client.PlayOpt(context.Background(), &opts)
-
+	opts := spotify.PlayOptions{DeviceID: &deviceid, URIs: []spotify.URI{result.Tracks.Tracks[0].URI}}
+	if queueSong {
+		err = client.QueueSongOpt(context.Background(), result.Tracks.Tracks[0].ID, &opts)
+	} else {
+		err = client.PlayOpt(context.Background(), &opts)
+	}
 	if err != nil {
 		return fmt.Errorf("Error while attempting to play: %w", err)
 	}
@@ -49,4 +54,5 @@ func SearchSongAndPlay(client *spotify.Client, deviceid spotify.ID, songname str
 }
 
 func init() {
+	SongCmd.Flags().BoolVarP(&queueSong, "queue", "q", true, "If this flag is set, the song will be added to the queue")
 }
